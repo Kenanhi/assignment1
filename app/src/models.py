e2dcbc32c452e69befaff119e2b808123550a35e
Kanan Huseynli <76840229+Kenanhi@users.noreply.github.com>
2025-10-21 05:16:38 +0200

    Add FastAPI assignment1 implementation

--- app/src/models.py
+++ app/src/models.py
@@ -1,17 +1,14 @@
 from typing import List, Optional
 from pydantic import BaseModel
 
-
 class Organizer(BaseModel):
-    email: str
     name: str
-
+    email: str
 
 class Joiner(BaseModel):
     name: str
-    country: str
     email: str
-
+    country: str
 
 class Event(BaseModel):
     id: Optional[int]
@@ -20,6 +17,7 @@ class Event(BaseModel):
     organizer: Organizer
     status: str
     type: str
-    max_attendees: int
     location: str
+    max_attendees: int
     joiners: Optional[List[Joiner]]
+ 
